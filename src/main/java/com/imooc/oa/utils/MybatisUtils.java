3c86a9e42c1a692197e5f08ac9764b8a6878db94
only <hwy113329@163.com>
2021-10-31 22:25:32 +0800

    完成OA项目中Mybatis的配置1.0

--- src/main/java/com/imooc/oa/utils/MybatisUtils.java
+++ src/main/java/com/imooc/oa/utils/MybatisUtils.java
@@ -1,6 +1,11 @@
 package com.imooc.oa.utils;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
 
 /**
  * MyBatisUtils工具类，创建全局唯一的SqlSessionFactory
@@ -8,4 +13,16 @@ import org.apache.ibatis.session.SqlSessionFactory;
 public class MybatisUtils {
     //利用static(静态)属于类不属于对象 ,保证全局唯一
     private static SqlSessionFactory sqlSessionFactory = null;
+    //static块用于初始化静态对象
+    static {
+        Reader reader = null;
+        try {
+            reader = Resources.getResourceAsReader("mybatis-config.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
+    }
 }
